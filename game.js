db731b89aac9ac3f95ad647414318cb718d432b2
K1taru <163011348+K1taru@users.noreply.github.com>
2025-05-19 02:30:17 +0800

    Update game.js on sound plays

--- game.js
+++ game.js
@@ -1,11 +1,18 @@
-
-
 const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
 let started = false;
 let level = 0;
+
+// Preload all sounds once
+const sounds = {
+  red: new Audio("sounds/red.mp3"),
+  blue: new Audio("sounds/blue.mp3"),
+  green: new Audio("sounds/green.mp3"),
+  yellow: new Audio("sounds/yellow.mp3"),
+  wrong: new Audio("sounds/wrong.mp3"),
+};
 
 $(document).keypress(function() {
   if (!started) {
@@ -17,13 +24,11 @@ $(document).keypress(function() {
 });
 
 $(".btn").click(function() {
-
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   animatePress(userChosenColour);
   playSound(userChosenColour);
-
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function animatePress(currentColour) {
@@ -34,9 +39,10 @@ function animatePress(currentColour) {
 }
 
 function playSound(colorName) {
-  let soundEffect = new Audio("sounds/" + colorName + ".mp3");
-  soundEffect.play();
-
+  if (sounds[colorName]) {
+    sounds[colorName].currentTime = 0;  // rewind to start to allow replaying quickly
+    sounds[colorName].play();
+  }
 }
 
 function nextSequence () {
@@ -50,7 +56,6 @@ function nextSequence () {
 
   animatePress(randomChosenColour);
   playSound(randomChosenColour);
-  
 }
 
 function checkAnswer(currentLevel) {
@@ -59,19 +64,15 @@ function checkAnswer(currentLevel) {
       console.log("level " + (currentLevel + 1) + " success");
       setTimeout(function() {
         nextSequence();
-      }, 1000);  
+      }, 1000);
     }
-
   } else {
     playSound("wrong");
     $("body").addClass("game-over");
-
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
-
     $("h1#level-title").text("Game Over, Press Any Key to Restart");
-    
     console.log("Game Over");
     startOver();
   }
